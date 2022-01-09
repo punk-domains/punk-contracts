62467f1773eae52f8f43906252586877e0b9657f
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-01-09 16:41:26 +0100

    mint returns token ID

--- contracts/Web3PandaTLD.sol
+++ contracts/Web3PandaTLD.sol
@@ -109,11 +109,11 @@ contract Web3PandaTLD is ERC721, Ownable {
   function mint(
     string memory _domainName, 
     address _domainOwner
-  ) public payable {
+  ) public payable returns(uint256) {
     require(buyingEnabled == true, "Buying TLDs is disabled");
     require(msg.value >= price, "Value below price");
 
-    _mintDomain(_domainName, _domainOwner, "", "", address(0), 0);
+    return _mintDomain(_domainName, _domainOwner, "", "", address(0), 0);
   }
 
   // mint with both mandatory and optional params
@@ -124,11 +124,11 @@ contract Web3PandaTLD is ERC721, Ownable {
     string memory _url,
     address _pfpAddress,
     uint256 _pfpTokenId
-  ) public payable {
+  ) public payable returns(uint256) {
     require(buyingEnabled == true, "Buying TLDs is disabled");
     require(msg.value >= price, "Value below price");
 
-    _mintDomain(_domainName, _domainOwner, _description, _url, _pfpAddress, _pfpTokenId);
+    return _mintDomain(_domainName, _domainOwner, _description, _url, _pfpAddress, _pfpTokenId);
   }
 
   function _mintDomain(
@@ -138,8 +138,8 @@ contract Web3PandaTLD is ERC721, Ownable {
     string memory _url,
     address _pfpAddress,
     uint256 _pfpTokenId
-  ) internal validName(_domainName) {
-    uint tokId = totalSupply;
+  ) internal validName(_domainName) returns(uint256) {
+    uint256 tokId = totalSupply;
     totalSupply++;
 
     _safeMint(_domainOwner, tokId);
@@ -161,7 +161,36 @@ contract Web3PandaTLD is ERC721, Ownable {
     domainIdsNames[tokId] = _domainName;
     
     emit DomainCreated(msg.sender, _domainOwner, fullDomainName);
+
+    return tokId;
   }
+
+
+  // check if holder of a domain (based on domain token ID) still owns their chosen pfp
+  // anyone can do this validation for any user
+  function validatePfp(uint256 _tokenId) public {
+    address pfpAddress = domains[domainIdsNames[_tokenId]].pfpAddress;
+    address holder = domains[domainIdsNames[_tokenId]].holder;
+
+    if (pfpAddress != address(0)) {
+      uint256 pfpTokenId = domains[domainIdsNames[_tokenId]].pfpTokenId;
+
+      ERC721 pfpContract = ERC721(pfpAddress); // get PFP contract
+
+      if (pfpContract.ownerOf(pfpTokenId) != holder) {
+        // if user does not own that PFP, delete the PFP address from user's Domain struct 
+        // (PFP token ID can be left alone to save on gas)
+        domains[domainIdsNames[_tokenId]].pfpAddress = address(0);
+        emit PfpValidated(msg.sender, holder, false);
+      } else {
+        emit PfpValidated(msg.sender, holder, true); // PFP image is valid
+      }
+    }
+    
+  }
+
+
+  // HOOK
 
   /**
     * @dev Hook that is called before any token transfer. This includes minting
@@ -194,32 +223,15 @@ contract Web3PandaTLD is ERC721, Ownable {
     }
   }
 
-  // check if holder of a domain (based on domain token ID) still owns their chosen pfp
-  // anyone can do this validation for any user
-  function validatePfp(uint256 _tokenId) public {
-    address pfpAddress = domains[domainIdsNames[_tokenId]].pfpAddress;
-    address holder = domains[domainIdsNames[_tokenId]].holder;
-
-    if (pfpAddress != address(0)) {
-      uint256 pfpTokenId = domains[domainIdsNames[_tokenId]].pfpTokenId;
-
-      ERC721 pfpContract = ERC721(pfpAddress); // get PFP contract
-
-      if (pfpContract.ownerOf(pfpTokenId) != holder) {
-        // if user does not own that PFP, delete the PFP address from user's Domain struct 
-        // (PFP token ID can be left alone to save on gas)
-        domains[domainIdsNames[_tokenId]].pfpAddress = address(0);
-        emit PfpValidated(msg.sender, holder, false);
-      } else {
-        emit PfpValidated(msg.sender, holder, true); // PFP image is valid
-      }
-    }
-    
-  }
-
   // OWNER
 
-  // TODO function: create a new domain for a specified address for free
+  // create a new domain for a specified address for free
+  function ownerMintDomain(
+    string memory _domainName, 
+    address _domainOwner
+  ) public onlyOwner returns(uint256) {
+    return _mintDomain(_domainName, _domainOwner, "", "", address(0), 0);
+  }
 
   // change the payment amount for a new domain
   function changePrice(uint256 _price) public onlyOwner {
