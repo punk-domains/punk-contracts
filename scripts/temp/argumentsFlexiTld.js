483ef3a0c86110383e8439bae6ae045bf9b86ebe
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-05-12 10:13:29 +0200

    Flexi TLD deployment script

---
+++ scripts/temp/argumentsFlexiTld.js
@@ -0,0 +1,12 @@
+/* needed only for deployTld.js purposes */
+
+module.exports = [
+  ".test",
+  "TEST",
+  "0xb29050965A5AC70ab487aa47546cdCBc97dAE45D",
+  "100000000000000000",
+  false,
+  0,
+  "0xf81dfed588af7be10ba095fb9e31ce3333b8618c",
+  "0xf0ec43efd96a71fc01c6aecf9a4a241ec7845397"
+];
---
+++ scripts/temp/deployFlexiTld.js
@@ -0,0 +1,57 @@
+/*
+This script is needed to verify contracts that are generated through contract factory.
+
+How to use:
+- First deploy the Factory contract
+- Then in deployTld.js replace the owner and factory addresses with the ones you want
+- Then deploy the PunkTLD contract: npx hardhat run scripts/temp/deployFlexiTld.js --network polygonMumbai
+- Go to arguments.js file and enter correct addresses (and other data if changed in deployTld.js)
+- Verify it (see command line for the Verify command). Something like: npx hardhat verify --network polygonMumbai --constructor-args scripts/arguments.js <deployed-contract-address>
+- After successful verification, every TLD contract created through the factory will show contract code. 
+- The PunkTLD contract that you deployed using this script can be ignored (factory does not know about it). It
+was only needed for the verification purposes.
+*/
+
+const contractName1 = "FlexiPunkTLD";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // deploy contract1
+  const contract1 = await ethers.getContractFactory(contractName1);
+  
+  const tldName = ".test";
+  const tldSymbol = "TEST";
+  const tldOwner = "0xb29050965A5AC70ab487aa47546cdCBc97dAE45D" // "<enter-owner-address>";
+  const tldPrice = ethers.utils.parseUnits("0.1", "ether");
+  const buyingEnabled = false;
+  const royalty = 0;
+  const factoryAddress = "0xf81dfed588af7be10ba095fb9e31ce3333b8618c" // "<enter-factory-address>";
+  const metadataAddress = "0xf0ec43efd96a71fc01c6aecf9a4a241ec7845397"; // "<enter-metadata-address>";
+
+  const instance1 = await contract1.deploy(
+    tldName, tldSymbol, tldOwner, tldPrice, buyingEnabled, royalty, factoryAddress, metadataAddress
+  );
+
+  console.log("Contract address:", instance1.address);
+
+  console.log("Wait a minute and then run this command:");
+  console.log(
+    "npx hardhat verify --network " + network.name + " --constructor-args scripts/temp/argumentsFlexiTld.js " + instance1.address
+  );
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
