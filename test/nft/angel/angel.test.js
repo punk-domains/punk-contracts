23dbdabcf913c9a2e40df6cd7a669a4356b56d5a
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-05-27 19:19:10 +0200

    Punk Angel NFT + whitelist (not completed yet)

--- test/nft/angel/angel.test.js
+++ test/nft/angel/angel.test.js
@@ -17,36 +17,50 @@ function calculateGasCosts(testName, receipt) {
   console.log(testName + " gas cost (Polygon): $" + String(Number(gasCostMatic)*matic));
 }
 
-describe("Punk Angel NFT", function () {
+describe("Punk Angel Metadata", function () {
   let contract;
   let metadataContract;
+  let usdcContract;
   let signer;
   let anotherUser;
 
   const provider = waffle.provider;
 
+  const nftName = "Punk Angel";
+  const nftSymbol = "PDANGEL";
+  const nftIdCounter = 1;
+
+  const usdcDecimals = 6;
+  const nftPrice = ethers.utils.parseUnits("500", usdcDecimals); // NFT price is in USDC (mwei, 6 decimals)
+
   beforeEach(async function () {
     [signer, anotherUser] = await ethers.getSigners();
 
-    //const PunkForbiddenTlds = await ethers.getContractFactory("PunkForbiddenTlds");
-    //const forbTldsContract = await PunkForbiddenTlds.deploy();
+    // mock USDC contract
+    const Erc20ContractDecimals = await ethers.getContractFactory("MockErc20TokenDecimals");
+    usdcContract = await Erc20ContractDecimals.deploy("USD Coin", "USDC", usdcDecimals); // 1000 USDC were minted for signer
 
+    // metadata contract
     const PunkAngelMetadata = await ethers.getContractFactory("PunkAngelMetadata");
     metadataContract = await PunkAngelMetadata.deploy();
+
+    // punk angel NFT contract
+    const PunkAngelNft = await ethers.getContractFactory("PunkAngelNft");
+    contract = await PunkAngelNft.deploy(
+      nftName,
+      nftSymbol,
+      nftIdCounter,
+      nftPrice,
+      metadataContract.address,
+      usdcContract.address
+    );
   });
 
-  it("should fetch and parse metadata", async function () {
-    const tokenId = 43;
-
-    const metadata = await metadataContract.getMetadata(tokenId);
-    
-    const mdJson = Buffer.from(metadata.substring(29), "base64");
-    const mdResult = JSON.parse(mdJson);
-
-    expect(mdResult.name).to.equal("Cyberpunk Angel #" + tokenId);
-    expect(mdResult.description).to.equal("A collection of Punk Angel NFTs."); 
-
-    console.log(mdResult.image);
+  it("should confirm NFT name & symbol", async function () {
+    const name = await contract.name();
+    expect(name).to.equal(nftName);
+    const symbol = await contract.symbol();
+    expect(symbol).to.equal(nftSymbol);
   });
 
 });
