3bca777806bca5babd1088623a18231101f8bbd9
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-06-09 12:50:39 +0200

    resolver: get domain holder

--- contracts/resolver/PunkResolverV1.sol
+++ contracts/resolver/PunkResolverV1.sol
@@ -16,6 +16,12 @@ contract PunkResolverV1 is Initializable, OwnableUpgradeable {
   mapping (address => bool) public isTldDeprecated; // deprecate an address, not TLD name
   address[] public factories;
 
+  /*
+  struct TldData {
+    address tldAddress;
+    string tldName;
+  }*/
+
   // initializer (only for V1!)
   function initialize() public initializer {
     __Context_init_unchained();
@@ -23,14 +29,41 @@ contract PunkResolverV1 is Initializable, OwnableUpgradeable {
   }
 
   // READ
+  function getDomainHolder(string calldata _domainName, string calldata _tld) public view returns(address) {
+    uint256 fLength = factories.length;
+    for (uint256 i = 0; i < fLength;) {
+      address tldAddr = IBasePunkTLDFactory(factories[i]).tldNamesAddresses(_tld);
+
+      if (tldAddr != address(0) && !isTldDeprecated[tldAddr]) {
+        return address(IBasePunkTLD(tldAddr).getDomainHolder(_domainName));
+      }
+
+      unchecked { ++i; }
+    }
+  }
+
   function getFactoriesArray() public view returns(address[] memory) {
     return factories;
   }
 
-  // notice: get a list of all active TLDs across all factories
-  //function getTldsArray() public view returns(address[] memory) {
+  /// @notice get a list of all active TLDs across all factories
+  /*
+  function getTldAddressesArray() public view returns(address[] memory) {
     // TODO: address or string or both? (struct?)
-  //} 
+    address[] memory _tldAddresses = new address[](8); // TODO: count how many TLDs there are
+
+    uint256 fLength = factories.length;
+    for (uint256 i = 0; i < fLength;) {
+      string[] memory tldNames = IBasePunkTLDFactory(factories[i]).getTldsArray();
+
+      for (uint256 j = 0; j < tldNames.length; ++j) {
+        _tldAddresses.push(IBasePunkTLDFactory(factories[i]).tldNamesAddresses(tldNames[j]));
+      }
+
+      unchecked { ++i; }
+    }
+  } 
+  */
 
   // OWNER
   function addFactoryAddress(address _factoryAddress) external onlyOwner {
--- test/resolver/proxy.test.js
+++ test/resolver/proxy.test.js
@@ -3,14 +3,37 @@ const { expect } = require('chai');
  
 describe("Punk Resolver Proxy", function () {
   let contract;
+
   let forbTldsContract;
+
   let factoryContract1;
   let factoryContract2;
-  let factoryContract3;
+
+  let tldContract1;
+  let tldContract2;
+  let tldContract3;
 
   let signer;
   let user1;
   let user2;
+
+  // TLD1
+  const tldName1 = ".wagmi";
+  const domainSymbol1 = ".WAGMI";
+  const domainPrice1 = ethers.utils.parseUnits("1", "ether");
+  const domainRoyalty1 = 10; // royalty in bips (10 bips is 0.1%)
+  
+  // TLD2
+  const tldName2 = ".degen";
+  const domainSymbol2 = ".DEGEN";
+  const domainPrice2 = ethers.utils.parseUnits("2", "ether");
+  const domainRoyalty2 = 20; // royalty in bips (10 bips is 0.1%)
+
+  // TLD1
+  const tldName3 = ".ape";
+  const domainSymbol3 = ".APE";
+  const domainPrice3 = ethers.utils.parseUnits("3", "ether");
+  const domainRoyalty3 = 30; // royalty in bips (10 bips is 0.1%)
 
   beforeEach(async function () {
     [signer, user1, user2] = await ethers.getSigners();
@@ -27,11 +50,44 @@ describe("Punk Resolver Proxy", function () {
     const PunkTLDFactory = await ethers.getContractFactory("PunkTLDFactory");
     factoryContract1 = await PunkTLDFactory.deploy(ethers.utils.parseUnits("1", "ether"), forbTldsContract.address);
     factoryContract2 = await PunkTLDFactory.deploy(ethers.utils.parseUnits("2", "ether"), forbTldsContract.address);
-    factoryContract3 = await PunkTLDFactory.deploy(ethers.utils.parseUnits("3", "ether"), forbTldsContract.address);
 
     await forbTldsContract.addFactoryAddress(factoryContract1.address);
     await forbTldsContract.addFactoryAddress(factoryContract2.address);
-    await forbTldsContract.addFactoryAddress(factoryContract3.address);
+
+    // create TLD no. 1
+    await factoryContract1.ownerCreateTld(
+      tldName1,
+      domainSymbol1,
+      signer.address, // TLD owner
+      domainPrice1,
+      true // buying enabled
+    )
+
+    // create TLD no. 2
+    await factoryContract1.ownerCreateTld(
+      tldName2,
+      domainSymbol2,
+      signer.address, // TLD owner
+      domainPrice2,
+      true // buying enabled
+    )
+
+    // create TLD no. 3
+    await factoryContract2.ownerCreateTld(
+      tldName3,
+      domainSymbol3,
+      signer.address, // TLD owner
+      domainPrice3,
+      true // buying enabled
+    )
+
+    const tldAddress1 = await factoryContract1.tldNamesAddresses(tldName1);
+    const tldAddress2 = await factoryContract1.tldNamesAddresses(tldName2);
+    const tldAddress3 = await factoryContract2.tldNamesAddresses(tldName3);
+
+    tldContract1 = await hre.ethers.getContractAt("PunkTLD", tldAddress1);
+    tldContract2 = await hre.ethers.getContractAt("PunkTLD", tldAddress2);
+    tldContract3 = await hre.ethers.getContractAt("PunkTLD", tldAddress3);
   });
   
   it('adds and removes a factory address', async function () {
@@ -65,7 +121,42 @@ describe("Punk Resolver Proxy", function () {
     )).to.be.revertedWith('Ownable: caller is not the owner');
   });
 
-  it('', async function () {});
+  it('fetches a domain holder', async function () {
+    const domainName = "user1";
+
+    // check domain holder via TLD contract before
+    const domainHolderViaTldBefore = await tldContract1.getDomainHolder(domainName);
+    expect(domainHolderViaTldBefore).to.equal(ethers.constants.AddressZero);
+
+    // create domain name
+    await tldContract1.mint(
+      domainName, // domain name (without TLD)
+      user1.address, // domain owner
+      ethers.constants.AddressZero, // no referrer
+      {
+        value: domainPrice1 // pay  for the domain
+      }
+    );
+
+    // check domain holder via TLD contract after
+    const domainHolderViaTldAfter = await tldContract1.getDomainHolder(domainName);
+    expect(domainHolderViaTldAfter).to.equal(user1.address);
+
+    // check domain holder via Resolver before
+    const domainHolderViaResolverBefore = await contract.getDomainHolder(domainName, tldName1);
+    expect(domainHolderViaResolverBefore).to.equal(ethers.constants.AddressZero);
+
+    // add factory address to the resolver contract
+    await contract.addFactoryAddress(factoryContract1.address);
+
+    // check domain holder via Resolver after
+    const domainHolderViaResolverAfter = await contract.getDomainHolder(domainName, tldName1);
+    expect(domainHolderViaResolverAfter).to.equal(user1.address);
+
+    // check non-existing domain name via Resolver
+    const domainHolderViaResolverNonExisting = await contract.getDomainHolder("nonExistingDomain", tldName1);
+    expect(domainHolderViaResolverNonExisting).to.equal(ethers.constants.AddressZero);
+  });
 
 
   //it('', async function () {});
