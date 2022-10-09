5b7dfde5923e1f38febd8f0bd568d1d2b5674928
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-10-09 14:37:09 +0200

    SGB script minter contract

--- scripts/partners/sgb/callMethods.js
+++ scripts/partners/sgb/callMethods.js
@@ -1,10 +1,9 @@
 // npx hardhat run scripts/partners/sgb/callMethods.js --network songbird
 
 const tldAddress = "0xBDACF94dDCAB51c39c2dD50BffEe60Bb8021949a";
-
 const minterAddress = "0xA33dCbE04278706248891931537Dd56B795c3663";
-
 const metadataAddress = "0xF51F7a532a2AaDFE8E2320bf5BA8275503bB3789";
+const royaltyFeeReceiver = "";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -24,8 +23,15 @@ async function main() {
     "function changeDescription(address _tldAddress, string calldata _description) external"
   ]);
 
+  const minterInterface = new ethers.utils.Interface([
+    "function royaltyFeeReceiver() external view returns(address)",
+    "function ownerFreeMint(string memory, address) external returns(uint256 tokenId)",
+    "function changeRoyaltyFeeReceiver(address _newReceiver) external"
+  ]);
+
   const tldContract = new ethers.Contract(tldAddress, tldInterface, deployer);
   const metadataContract = new ethers.Contract(metadataAddress, metadataInterface, deployer);
+  const minterContract = new ethers.Contract(minterAddress, minterInterface, deployer);
 
   // CHANGE MINTER
 
@@ -35,8 +41,6 @@ async function main() {
   //await tldContract.changeMinter(minterAddress);
   //await minterContract.togglePaused();
   //await minterContract.transferOwnership(newOwnerAddress);
-  
-  console.log("Method call completed");
 
   const minterAfter = await tldContract.minter();
   console.log("Minter after: " + minterAfter);
@@ -50,6 +54,22 @@ async function main() {
     "The first web3 domain provider on Songbird. Mint your very own .sgb domain on https://songbird.domains/"
   );
   */
+
+  // CHANGE ROYATLY FEE RECEIVER
+  /*
+  const royaltyFeeReceiverBefore = await minterContract.royaltyFeeReceiver();
+  console.log("RoyaltyFeeReceiver before: " + royaltyFeeReceiverBefore);
+
+  await minterContract.changeRoyaltyFeeReceiver(royaltyFeeReceiver);
+
+  const royaltyFeeReceiverAfter = await minterContract.royaltyFeeReceiver();
+  console.log("RoyaltyFeeReceiver after: " + royaltyFeeReceiverAfter);
+  */
+
+  // FREE DOMAIN MINT
+  const freeDomainName = ""; // without domain extension
+  const freeDomainReceiver = ""; // 0x address
+  //await minterContract.ownerFreeMint(freeDomainName, freeDomainReceiver);
 }
 
 main()
