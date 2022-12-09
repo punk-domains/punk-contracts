fff8e51ca417d9912cb5e857347b76fac9e2be8e
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-12-09 13:11:07 +0100

    satrap custom metadata contract

---
+++ test/partners/satrap/satrap.test.js
@@ -0,0 +1,39 @@
+// npx hardhat test test/partners/satrap/satrap.test.js
+const { expect } = require("chai");
+
+describe(".satrap Metadata", function () {
+  let metadataContract;
+  let signer;
+
+  beforeEach(async function () {
+    [signer] = await ethers.getSigners();
+
+    const SatrapMetadata = await ethers.getContractFactory("SatrapMetadata");
+    metadataContract = await SatrapMetadata.deploy();
+  });
+
+  it("should fetch and parse metadata", async function () {
+    const tokenId = 1;
+    const domainName = "clavi";
+    const tld = ".satrap";
+
+    const metadata = await metadataContract.getMetadata(domainName, tld, tokenId);
+
+    //console.log(metadata)
+    
+    const mdJson = Buffer.from(metadata.substring(29), "base64");
+    const mdResult = JSON.parse(mdJson);
+
+    // metadata: 
+    //console.log(mdResult);
+    console.log(mdResult.name);
+    console.log(mdResult.attributes);
+
+    expect(mdResult.name).to.equal(domainName+tld);
+    
+    // SVG image:
+    console.log(mdResult.image);
+
+  });
+
+});
